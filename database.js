3a38b05b118324efca23401ed7ec9175c2b54ba3
Joseph Saunderson <06saundersonj@gmail.com>
2025-11-03 16:55:06 -0600

    Update app to database.js

--- database.js
+++ database.js
@@ -1,9 +1,13 @@
+import express from 'express'
 import mysql from 'mysql2'
 
+const app = express()
+app.use(express.json())
+
 const pool = mysql.createPool({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
-    password: '.....',
+    password: '',
     database: 'project'
 })
 
